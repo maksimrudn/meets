d22005f12fc1b2b1044785fdcb21e48254ffe8c2
Maksim Rudnev <m.rudneov@yandex.ru>
2022-10-07 10:04:12 +0300

    исправления

--- Meets/ClientApp/src/store/user.ts
+++ Meets/ClientApp/src/store/user.ts
@@ -25,15 +25,11 @@ interface ITabHeaderItem {
     count: number
 }
 
-export interface IUserState { // extends IUserCardResponse
+export interface IUserState { 
     user: UserCardResponse | null
     
     tabHeader: ITabHeaderItem[]
-    //tabInfo: ITabInfo
-    //tabLearnings: Learning[]
-    //tabWorks: Work[]
-    //tabActivities: Activity[]
-    //tabFacts: Fact[]
+
     isLoading: boolean
     error: string | null
     dataLoaded: boolean
@@ -41,45 +37,9 @@ export interface IUserState { // extends IUserCardResponse
 
 const initialState: IUserState = {
     user: null,
-    //id: 0,
-    //gender: '',
-    //birthDate: '',
-    //avatar: '',
-    //city: '',
-    //company: '',
-    //job: '',
-    //specialization: '',
-    //fullName: '',
-    //lockoutEnabled: false,
-    //tags: [],
-    //latitude: 0,
-    //longitude: 0,
-    //description: '',
-    //growth: 0,
-    //weight: 0,
-    //subscribers: 0,
-    //subscriptions: 0,
-    //learnings: [],
-    //works: [],
-    //activities: [],
-    //facts: [],
-    //distance: 0,
-    //isSubscribed: false,
-    //friendRequestIsRejected: false,
-    //meetings: 0,
-    //isInvited: false,
+ 
     tabHeader: [],
-    //tabInfo: {
-    //    birthDate: '',
-    //    city: '',
-    //    description: '',
-    //    growth: 0,
-    //    weight: 0
-    //},
-    //tabLearnings: [],
-    //tabWorks: [],
-    //tabActivities: [],
-    //tabFacts: [],
+
     isLoading: false,
     error: null,
     dataLoaded: false 
@@ -94,33 +54,7 @@ const userSlice = createSlice({
         },
         userReceived: (state, action) => {
             state.user = action.payload;
-            //state.id = action.payload.id;
-            //state.gender = action.payload.gender;
-            //state.birthDate = action.payload.birthDate;
-            //state.avatar = action.payload.avatar;
-            //state.city = action.payload.city;
-            //state.company = action.payload.company;
-            //state.job = action.payload.job;
-            //state.specialization = action.payload.specialization;
-            //state.fullName = action.payload.fullName;
-            //state.lockoutEnabled = action.payload.lockoutEnabled;
-            //state.tags = action.payload.tags;
-            //state.latitude = action.payload.latitude;
-            //state.longitude = action.payload.longitude;
-            //state.description = action.payload.description;
-            //state.growth = action.payload.growth;
-            //state.weight = action.payload.weight;
-            //state.subscribers = action.payload.subscribers;
-            //state.subscriptions = action.payload.subscriptions;
-            //state.learnings = action.payload.learnings;
-            //state.works = action.payload.works;
-            //state.activities = action.payload.activities;
-            //state.facts = action.payload.facts;
-            //state.distance = action.payload.distance;
-            //state.isSubscribed = action.payload.isSubscribed;
-            //state.friendRequestIsRejected = action.payload.friendRequestIsRejected;
-            //state.meetings = action.payload.meetings;
-            //state.isInvited = action.payload.isInvited;
+            
             state.tabHeader = [
                 {
                     title: UserCardTabsNames.Info,
@@ -143,11 +77,7 @@ const userSlice = createSlice({
                     count: action.payload.facts?.length
                 }
             ];
-            //state.tabInfo = action.payload;
-            //state.tabLearnings = action.payload.learnings;
-            //state.tabWorks = action.payload.works;
-            //state.tabActivities = action.payload.activities;
-            //state.tabFacts = action.payload.facts;
+           
             state.isLoading = false;
             state.dataLoaded = true;
             state.error = null;
@@ -157,9 +87,6 @@ const userSlice = createSlice({
             state.isLoading = false;
             state.dataLoaded = true;
         },
-    //    userEdited: (state, action) => {
-
-    //    }
     }
 });
 
