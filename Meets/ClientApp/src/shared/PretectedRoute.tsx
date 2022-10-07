489959767d9fd8c38c0258b0ddae8c4aedad27ff
Administrator <Administrator@VW10-WRK-DEV-2>
2022-10-07 10:18:18 +0300

    es-447 protected router

---
+++ Meets/ClientApp/src/shared/PretectedRoute.tsx
@@ -0,0 +1,17 @@
+﻿import React, { Component, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, NavLink } from "react-router-dom";
+import useAccountStore from '../hooks/useAccountStore';
+
+
+
+
+const ProtectedRoute = ({ ...props }) => {
+
+    const accountStore = useAccountStore();
+
+
+    return accountStore.isSignedIn ?
+        <Route {...props} /> : <Redirect to={'/'} />
+};
+
+export default ProtectedRoute;
